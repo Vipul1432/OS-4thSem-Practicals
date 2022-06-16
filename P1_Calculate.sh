e7a4335783299f85ccdaf288d3fb8cba9db333fd
Vipul <81670997+Vipul1432@users.noreply.github.com>
2022-06-16 11:08:34 +0530

    Add-Sub-Mul-Div

---
+++ P1_Calculate.sh
@@ -0,0 +1,7 @@
+echo "enter two num"
+read a
+read b
+echo "addition of two numbers: `expr $a + $b`"
+echo "division of two numbers: ` expr $a / $b`"
+echo "substraction of two numbers: ` expr $a - $b `"
+echo "multiplication of two numbers: ` expr $a \* $b`"
