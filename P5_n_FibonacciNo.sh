c436681f0f2efdb143857042863e786751574cd9
Vipul <81670997+Vipul1432@users.noreply.github.com>
2022-06-16 11:19:08 +0530

    n_FibonacciNo

---
+++ P5_n_FibonacciNo.sh
@@ -0,0 +1,16 @@
+echo How many number of terms to be generated ?
+read n
+x=0
+y=1
+i=2
+echo Fibonacci Series up to $n terms :
+echo $x
+echo $y
+while [ $i -lt $n ]
+do
+    i=`expr $i + 1 `
+    z=`expr $x + $y `
+    echo "$z"
+    x=$y
+    y=$z
+done
