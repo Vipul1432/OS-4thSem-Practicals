2d5bfda5a74d25f33cea7e3c23a7703a5ed69f6a
Vipul <81670997+Vipul1432@users.noreply.github.com>
2022-06-16 11:09:14 +0530

    Swap No

---
+++ P1_swapNo.sh
@@ -0,0 +1,9 @@
+echo "enter First number: "
+read first
+echo "enter second number: "
+read second
+temp=$first
+first=$second
+second=$temp
+echo "After swapping two numbers: "
+echo "first number = $first and second number = $second"
