935416dbd6bd9a893976dcac36d652c3111e7a66
Vipul <81670997+Vipul1432@users.noreply.github.com>
2022-06-16 11:19:22 +0530

    P5_n_primeNo

---
+++ P5_n_primeNo.sh
@@ -0,0 +1,22 @@
+echo “enter the range”
+read n
+echo “the prime no are:”
+m=2
+while [ $m -le $n ]
+do
+    i=2 flag=0
+    while [ $i -le `expr $m / 2` ]
+    do
+        if [ `expr $m % $i` -eq 0 ]
+        then
+            flag=1
+            break
+        fi
+        i=`expr $i + 1`
+    done
+    if [ $flag -eq 0 ]
+    then
+        echo $m
+    fi
+    m=`expr $m + 1`
+done
