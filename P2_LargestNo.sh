dfdfcbbb2f79bda6090ea0331742d9b160da8fbc
Vipul <81670997+Vipul1432@users.noreply.github.com>
2022-06-16 11:10:28 +0530

    Largest No

---
+++ P2_LargestNo.sh
@@ -0,0 +1,15 @@
+echo “Enter Number 1 :”
+read num1
+echo “Enter Number 2 :”
+read num2
+echo “Enter Number 3 :”
+read num3
+if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ]
+then
+    echo “ $num1”
+elif [ $num2 -gt $num1 ] && [ $num2 -gt $num3 ]
+then
+    echo “ $num2”
+else
+    echo “ $num3”
+fi
